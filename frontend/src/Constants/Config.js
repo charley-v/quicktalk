c6898bec5855c87f59620d9941287c7f63502818
Charley <charleyvieyra143@gmail.com>
2024-11-29 13:32:11 -0800

    Update

--- frontend/src/Constants/Config.js
+++ frontend/src/Constants/Config.js
@@ -1,7 +1,7 @@
 export const GLOBAL_CONFIG = {
-  clientId: "742te01c7g4flltv51m38keelk", // Cognito App Client ID
+  clientId: "", // Cognito App Client ID
   redirectUri: "http://localhost:3000/callback", // Redirect URI
-  clientSecret:"1k6jvfs7idrmv3g6i4mjso7h3kjkb2r8qfhn8jfrghpjr0qapl51",
-  cognitoDomain: "us-east-1eslqtmoz9.auth.us-east-1.amazoncognito.com",
+  clientSecret:"",
+  cognitoDomain: "",
   backendUrl: "http://localhost:8080"
 };
